553e34e82c6584370f1e12b36adf0dda4051d893
Manuel Ortega <mortegac@gmail.com>
2025-05-08 23:09:08 -0400

    add google tag manager'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,32 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
 import { Inter } from 'next/font/google';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
+import GoogleTagManager from '@/components/GoogleTagManager';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const locale = await getLocale();
   const messages = await getMessages();
+  const gtmId = "GTM-K9KD3K78";
+  // const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
 
   return (
     <html lang={locale} suppressHydrationWarning={true}>
+      <head>
+        <GoogleTagManager />
+      </head>
       <body className={`${inter.className} flex min-h-screen flex-col dark:bg-[#151F34]`}>
+        {gtmId && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+        )}
         <NextIntlClientProvider messages={messages}>
           <GoogleAnalytics />
           {children}
---
+++ src/components/GoogleTagManager.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function GoogleTagManager() {
+//   const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  const gtmId = "GTM-K9KD3K78";
+
+  if (!gtmId) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtmId}');
+        `}
+      </Script>
+    </>
+  );
+} 
